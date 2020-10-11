f5f5f7e85f3da3e32ee1018a918f14e512c853c2
d-jo <declan.l.johnson@gmail.com>
2020-10-11 15:15:12 -0500

    Init commit

--- batch-process1.sh
+++ batch-process1.sh
@@ -1,9 +1,13 @@
 #! /bin/sh 
+TOTAL=0
 for f in "$1"/*; do
+	echo "$f"
   if [ -f "$f" ]; then
     SIZE="$(du -sh "${f}" | cut -f1)"
     echo "Processing $f file..."
     echo "$SIZE"
+		TOTAL=$(($TOTAL+$SIZE))
+		echo "$TOTAL"
   fi
 done
 
